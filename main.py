fdf3a8c505ae959bcfd5b1f6d876c4eb546c5a41
Brett Bertrand <brett.bertrand@frontier.com>
2022-01-16 14:45:16 -0800

    Homescreen launches

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import sys
 
-from PyQt5.QtCore import Qt
+from PyQt5 import QtWidgets
 
 from homescreen import HomeScreen
 
 
 if __name__ == '__main__':
     # Create the application and its main window
-    application = Qt.QGuiApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
 
     # Initialize Homescreen
     window = HomeScreen()
+    window.show()
+    sys.exit(app.exec_())
